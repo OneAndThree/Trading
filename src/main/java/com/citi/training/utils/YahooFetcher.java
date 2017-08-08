836d5558455a3ca52e052cf4df4d355f612088ff
424590883 <424590883@qq.com>
2017-08-08 09:56:46 +0800

    delete stock class

--- src/main/java/com/citi/training/utils/YahooFetcher.java
+++ src/main/java/com/citi/training/utils/YahooFetcher.java
@@ -1,20 +1,13 @@
 package com.citi.training.utils;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import com.citi.training.dao.StockData;
 
 
 /**
